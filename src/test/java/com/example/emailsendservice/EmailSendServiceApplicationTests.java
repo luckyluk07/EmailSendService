44a9a58d2a18575fd8b9c40872d76199f3fa82c3
luckyluk07 <L.Nojman11@wp.pl>
2022-03-17 22:34:15 +0100

    Initial commit

---
+++ src/test/java/com/example/emailsendservice/EmailSendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emailsendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSendServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
