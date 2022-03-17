427b22b32d2a43a5422139e3d7923d491e075bf8
luckyluk07 <L.Nojman11@wp.pl>
2022-03-17 23:04:01 +0100

    Add data initializer

--- src/main/java/com/example/emailsendservice/DataInitializer.java
+++ src/main/java/com/example/emailsendservice/DataInitializer.java
@@ -1,4 +1,27 @@
 package com.example.emailsendservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
 public class DataInitializer {
+    private UserRepository repository;
+
+    @Autowired
+    public DataInitializer(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostConstruct
+    public void initUsers() {
+        User user1 = User.builder()
+                .username("FirstUser")
+                .email("first.user@gmail.com")
+                .build();
+
+        this.repository.save(user1);
+    }
 }
