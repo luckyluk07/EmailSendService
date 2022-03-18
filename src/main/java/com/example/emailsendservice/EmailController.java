75065e355511659d7ffd556de243abfe7f832f9a
luckyluk07 <L.Nojman11@wp.pl>
2022-03-18 14:48:07 +0100

    Add findById to emailController

--- src/main/java/com/example/emailsendservice/EmailController.java
+++ src/main/java/com/example/emailsendservice/EmailController.java
@@ -1,7 +1,9 @@
 package com.example.emailsendservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,14 @@ public class EmailController {
     @GetMapping
     public List<User> findAll() {
         return this.emailService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = this.emailService.findById(id);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
     }
 }
--- src/main/java/com/example/emailsendservice/EmailService.java
+++ src/main/java/com/example/emailsendservice/EmailService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmailService {
@@ -16,5 +17,13 @@ public class EmailService {
 
     public List<User> findAll() {
         return this.userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        if (user.isPresent()) {
+            return user.get();
+        }
+        return null;
     }
 }
